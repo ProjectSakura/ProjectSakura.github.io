c6e63db84064f8d311808190a50144b9e7d6a7d8
ArunTeltia <teltia.arun18@gmail.com>
2020-07-29 20:05:28 +0530

    mantainer added changelog in downloads

---
+++ changelog.js
@@ -0,0 +1,345 @@
+var btn1 = $(".show-modal1")
+var btn2 = $(".show-modal2")
+var btn3 = $(".show-modal3")
+var btn4 = $(".show-modal4")
+var btn5 = $(".show-modal5")
+var btn6 = $(".show-modal6")
+var btn7 = $(".show-modal7")
+var btn8 = $(".show-modal8")
+var btn9 = $(".show-modal9")
+var btn10 = $(".show-modal10")
+var btn11 = $(".show-modal11")
+var btn12 = $(".show-modal12")
+var btn13 = $(".show-modal13")
+var btn14 = $(".show-modal14")
+var btn15 = $(".show-modal15")
+var btn16 = $(".show-modal16")
+var btn17 = $(".show-modal17")
+
+
+
+btn1.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "ASUS ZENFONE MAX PRO M1";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_x00t.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+
+    }
+    await showmodal1()
+})
+btn2.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "HUAWEI NEXUS 6P";
+
+
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_angler.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+
+    }
+    await showmodal1()
+})
+btn3.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "LEECO LE 2";
+
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_s2.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+
+    }
+    await showmodal1()
+})
+btn4.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "MEIZU 16TH";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_m1882.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+    }
+    await showmodal1()
+})
+btn5.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "MEIZU 16T";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_m1928.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+
+    }
+    await showmodal1()
+})
+btn6.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "MOTOROLA G7 POWER";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_ocean.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+
+    }
+    await showmodal1()
+})
+btn7.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "MOTOROLA G5S PlUS";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_sanders.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+
+    }
+    await showmodal1()
+})
+btn8.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "REALME 1";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_cph1859.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+
+    }
+    await showmodal1()
+})
+btn9.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "SAMSUNG GALAXY S8";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_dreamlte.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+
+    }
+    await showmodal1()
+})
+btn10.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "SAMSUNG GALAXY S8 PLUS";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_dream2lte.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+
+    }
+    await showmodal1()
+})
+btn11.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "SAMSUNG GALAXY NOTE 8";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_greatlte.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+
+    }
+    await showmodal1()
+})
+btn12.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "XIAOMI MI 6X";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_wayne.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+
+    }
+    await showmodal1()
+})
+btn13.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "XIAOMI POCOFONE F1";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_beryllium.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+
+    }
+    await showmodal1()
+
+})
+btn14.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "XIAOMI REDMI 5A";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_riva.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+
+    }
+    await showmodal1()
+})
+btn15.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "XIAOMI REDMI NOTE 4 ";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_mido.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+        // console.log($("#testmodal"))
+
+    }
+    await showmodal1()
+})
+console.log(btn16)
+btn16.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "XIAOMI REDMI NOTE 7 PRO";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_violet.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+
+    }
+    await showmodal1()
+})
+btn17.click(async () => {
+
+    async function showmodal1() {
+        $("#testmodal").modal('show');
+        const mobile_name = document.querySelector(".mobile_name");
+        mobile_name.innerText = "MOTOROLA G3 TURBO";
+        console.log("hello")
+        const url1 = 'https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_merlin.txt'
+        const response = await fetch(url1);
+
+        const data = await response.text();
+        console.log(data);
+        const para = document.querySelector(".data-modal");
+        para.innerText = data
+
+    }
+    await showmodal1()
+})
