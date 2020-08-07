ccc571820005a1344cda72587fd1a23bdc4cf1d9
ArunTeltia <teltia.arun18@gmail.com>
2020-08-07 22:24:34 +0530

    logs removed + minisweeper added on download click+river data fetched+ added changelog for meizu 16X

--- js/filter.js
+++ js/filter.js
@@ -2,14 +2,10 @@
 function filter() {
     var input = document.getElementById('search123');
     var FilterValue = input.value.toUpperCase();
-
     var ul = document.getElementById('search-ul');
     var li = ul.getElementsByClassName('search-li');
-    console.log(li);
     for (let i = 0; i < li.length; i++) {
-        // console.log(li[i].getElementsByTagName('div')[0])
         var a = li[i].getElementsByTagName('div')[0];
-        console.log(a.innerText);
         if (a.innerText.toUpperCase().indexOf(FilterValue) > -1) {
             li[i].style.display = "";
         } else {
