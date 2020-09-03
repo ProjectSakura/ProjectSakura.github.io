9d5d378450d2d2f973f41eeb1d5612a2332c1d25
ArunTeltia <teltia.arun18@gmail.com>
2020-09-03 11:13:00 +0530

    refractor the download page

---
+++ mantainers.js
@@ -0,0 +1,206 @@
+const data = {
+    "devices": [
+        {
+            "name": "LordShenron",
+            "device_name": "ASUS ZENFONE MAX PRO M1",
+            "alternate_name": "X00T",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Andrzej",
+            "device_name": "GOOGLE PIXEL",
+            "alternate_name": "sailfish",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Andrzej",
+            "device_name": "GOOGLE PIXEL XL",
+            "alternate_name": "marlin",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Amartya",
+            "device_name": "HUAWEI NEXUS 6P ",
+            "alternate_name": "angler",
+            "IsDropped": "(SUPPORT DROPPED)",
+            "droppedMessage": "THE SUPPORT FOR THIS DEVICE IS DROPPED, DOWNLOAD AT YOUR OWN RISK."
+
+        }
+        ,
+        {
+            "name": "CakesTwix",
+            "device_name": "LEECO LE 2",
+            "alternate_name": "s2",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Abdibekov Dauren",
+            "device_name": "MEIZU 16TH",
+            "alternate_name": "m1882",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Abdibekov Dauren",
+            "device_name": "MEIZU 16T",
+            "alternate_name": "m1928",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Alex",
+            "device_name": "MEIZU 16X",
+            "alternate_name": "m1872",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Vitor",
+            "device_name": "MOTOROLA G7 POWER",
+            "alternate_name": "ocean",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Vitor",
+            "device_name": "MOTOROLA G7",
+            "alternate_name": "river",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "ARMSM",
+            "device_name": "MOTOROLA G5S PLUS",
+            "alternate_name": "sanders",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "CHARLIE-117",
+            "device_name": "MOTOROLA G3 TURBO",
+            "alternate_name": "merlin",
+            "IsDropped": "(SUPPORT DROPPED)",
+            "droppedMessage": "THE SUPPORT FOR THIS DEVICE IS DROPPED, DOWNLOAD AT YOUR OWN RISK."
+
+        }
+        ,
+        {
+            "name": "AQUARIUSJR10",
+            "device_name": "ONEPLUS 3 AND 3T ",
+            "alternate_name": "oneplus3",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "MrMobTech",
+            "device_name": "REALME 1",
+            "alternate_name": "CPH1859",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Haridhayal",
+            "device_name": "SAMSUNG GALAXY S8",
+            "alternate_name": "dreamlte",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Haridhayal",
+            "device_name": "SAMSUNG GALAXY S8 PLUS",
+            "alternate_name": "dream2lte",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Haridhayal",
+            "device_name": "SAMSUNG GALAXY NOTE 8",
+            "alternate_name": "greatlte",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Roger W.",
+            "device_name": "XIAOMI MI 6X",
+            "alternate_name": "wayne",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Roger W.",
+            "device_name": "XIAOMI MI A2",
+            "alternate_name": "jasmine",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "HumanGlitch",
+            "device_name": "XIAOMI POCOFONE F1",
+            "alternate_name": "beryllium",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "SunnyRaj",
+            "device_name": "XIAOMI REDMI 5A",
+            "alternate_name": "riva",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "ThatisJigen",
+            "device_name": "XIAOMI REDMI NOTE 4",
+            "alternate_name": "mido",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Darkstar",
+            "device_name": "XIAOMI REDMI NOTE 5 PRO",
+            "alternate_name": "whyred",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Prajwal & Nitish",
+            "device_name": "XIAOMI REDMI NOTE 7 PRO",
+            "alternate_name": "violet",
+            "IsDropped": "",
+            "droppedMessage": ""
+        }
+        ,
+        {
+            "name": "Amartya",
+            "device_name": "XIAOMI REDMI NOTE 8/8T",
+            "alternate_name": "ginkgo",
+            "IsDropped": "",
+            "droppedMessage": ""
+
+        }
+    ]
+}
