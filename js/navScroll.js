e606bb7698a5d45037ba3c5772dd847381b74ebb
anujarora0502 <anujarora0502@gmail.com>
2020-09-09 17:05:58 +0530

    scroll to the hask links feature added

---
+++ js/navScroll.js
@@ -0,0 +1,11 @@
+$("a.scrolltolink").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
