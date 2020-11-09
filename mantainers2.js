998ac3fddc1aab502da3c7a7983aecc40f700bf3
Arun Teltia <43555219+ArunTeltia@users.noreply.github.com>
2020-11-09 21:08:49 +0530

    Android 11
    * Android 11 updates
    
    * some bugs fixed
    
    * 2 varients of samsung added

---
+++ mantainers2.js
@@ -0,0 +1,60 @@
+const data2 = {
+  devices: [
+    {
+      name: "LordShenron",
+      device_name: "ASUS ZENFONE MAX PRO M1",
+      alternate_name: "X00T",
+      IsDropped: "",
+      droppedMessage: "",
+    },
+    {
+      name: "CakesTwix",
+      device_name: "LEECO LE 2",
+      alternate_name: "s2",
+      IsDropped: "",
+      droppedMessage: "",
+    },
+    {
+      name: "Haridhayal",
+      device_name: "SAMSUNG GALAXY A30",
+      alternate_name: "a30",
+      IsDropped: "",
+      droppedMessage: "",
+    },
+    {
+      name: "Haridhayal",
+      device_name: "SAMSUNG GALAXY A40",
+      alternate_name: "a40",
+      IsDropped: "",
+      droppedMessage: "",
+    },
+    {
+      name: "Haridhayal",
+      device_name: "SAMSUNG GALAXY M20",
+      alternate_name: "m20lte",
+      IsDropped: "",
+      droppedMessage: "",
+    },
+    {
+      name: "Haridhayal",
+      device_name: "SAMSUNG GALAXY M30",
+      alternate_name: "m30lte",
+      IsDropped: "",
+      droppedMessage: "",
+    },
+    {
+      name: "Wally & Nitish",
+      device_name: "XIAOMI REDMI NOTE 7 PRO",
+      alternate_name: "violet",
+      IsDropped: "",
+      droppedMessage: "",
+    },
+    {
+      name: "A9ito",
+      device_name: "XIAOMI REDMI NOTE 9 PRO (GLOBAL ONLY)",
+      alternate_name: "joyeuse",
+      IsDropped: "",
+      droppedMessage: "",
+    },
+  ],
+};
