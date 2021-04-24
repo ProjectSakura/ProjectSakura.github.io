e306fe3bd67b0ff0323329ad81eb3ed0005ddc67
Bhavya-Sehgal <66208607+Bhavya-Sehgal@users.noreply.github.com>
2021-04-24 21:07:00 +0530

    Scroll-top added to website (#283)

--- js/timeline.js
+++ js/timeline.js
@@ -18,3 +18,19 @@ tl.from(
   },
   "=-2.2"
 );
+
+mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
